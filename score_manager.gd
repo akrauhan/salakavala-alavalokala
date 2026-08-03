62681d2467961796cd5ab0555404d4f1cd9ff48f
akrauhan <a.rauhansalo@gmail.com>
2026-08-03 19:20:48 +0300

    fixed scoreboard crash when hit

--- score_manager.gd
+++ score_manager.gd
@@ -4,8 +4,7 @@ var scores := {}
 
 @export var game_over_timer: Timer
 
-@onready var scoreboard = $"../MarginContainer/Scoreboard"
-
+var scoreboard
 
 func add_player(player_id):
 	scores[player_id] = 0
@@ -18,10 +17,6 @@ func add_score(player_id, amount):
 	
 func get_score(player_id):
 	return scores.get(player_id, 0)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 func game_over(player_id):
 	print("Player ", player_id, " won")
