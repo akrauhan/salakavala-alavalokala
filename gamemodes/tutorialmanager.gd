61771b4332bec564e34e4b28391880b1d699072c
akrauhan <a.rauhansalo@gmail.com>
2026-08-03 18:49:53 +0300

    tutorial

--- gamemodes/tutorialmanager.gd
+++ gamemodes/tutorialmanager.gd
@@ -2,16 +2,30 @@ extends Node2D
 
 @onready var label: Label = $"UI/TutorialLabel"
 @onready var playermanager = $PlayerManager
+@onready var progress_bar = $UI/ProgressBar
 
 @export var decoy_scene: PackedScene
 var decoy
 
 var step := 0
 var players := 0
-var aim_progress := 0.0
+var aim_progress := {}
+var light_toggles := {}
+var dashed_players := {}
+var bitten_players := {}
+var final_bites := {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	players = playermanager.players_container.get_children().size()
+	
+	for player in playermanager.players_container.get_children():
+		aim_progress[player.player_id] = 0.0
+		light_toggles[player.player_id] = 0
+		dashed_players[player.player_id] = false
+		bitten_players[player.player_id] = false
+		var bite = player.get_node("Bite")
+		bite.bite_performed.connect(_on_bite_performed)
+		
 	next_step()
 	
 
@@ -20,16 +34,22 @@ func next_step():
 
 	match step:
 		1:
+			progress_bar.value = 0
 			show_message("Move with the left stick.")
 		2:
+			progress_bar.value = 0
 			show_message("Aim your light with the right stick")
 		3:
+			progress_bar.value = 0
 			show_message("Press LB to toggle light on or off.")
 		4:
+			progress_bar.value = 0
 			show_message("Press LT to dash into the direction you are moving.")
 		5:
+			progress_bar.value = 0
 			show_message("Press RT to bite towards your light.")
 		6: 
+			progress_bar.value = 0
 			setup_final_step()
 			show_message("Bite the decoy-fish in the middle to finish the tutorial.")
 
@@ -38,25 +58,16 @@ func _process(delta: float) -> void:
 	match step:
 		1:
 			update_movement_progress()
-	
 		2:
-			
-			var input := Vector2(
-				Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
-				Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
-			)
-
-			if input.length() > 0.5:
-				aim_progress += delta
-
-			if aim_progress >= 2.0:
-				next_step()
+			update_aim_progress(delta)
 		3:
 			update_light_progress()
 		4:
 			update_dash_progress()
 		5:
 			update_bite_progress()
+		6:
+			update_final_progress()
 	
 	
 func show_message(text):
@@ -65,67 +76,145 @@ func show_message(text):
 
 	var tween = create_tween()
 	tween.tween_property(label, "modulate:a", 1.0, 0.3)
-	
 
 func update_movement_progress():
-	var amount = players_moved()
+	var step_goal := 200.0
+	var movements := players_moved()
 
-	if amount >= 100*players: 
+	var completed := 0
+	
+	var sum = 0
+	for player_id in movements:
+		var progress = min(movements[player_id], step_goal)
+		sum += progress
+		if movements[player_id] >= step_goal:
+			completed += 1
+
+	progress_bar.value = sum / (players * step_goal)
+
+	if completed >= players:
 		next_step()
 
-func players_moved():
-	var total := 0
-	
+
+func players_moved() -> Dictionary:
+	var player_progress = {}
 	for player in playermanager.players_container.get_children():
-		total += player.global_position.distance_to(player.start_pos)
-	
-	return total
+		player_progress[player.player_id] = player.global_position.distance_to(player.start_pos)
+	return player_progress
 
 
 func update_light_progress():
-	var pressed := false
+	var required_toggles := 1
+	var completed := 0
+	var total_progress := 0.0
 
 	for player in playermanager.players_container.get_children():
-		if Input.is_joy_button_pressed(player.player_id, JOY_BUTTON_LEFT_SHOULDER):
-			pressed = true
+		var pressed := Input.is_joy_button_pressed(
+			player.player_id,
+			JOY_BUTTON_LEFT_SHOULDER
+		)
 
-	if pressed:
+		# Detect button press, not button hold
+		if pressed:
+			light_toggles[player.player_id] += 1
+
+		
+		var player_progress = min(light_toggles[player.player_id], required_toggles)
+		total_progress += player_progress
+
+		if light_toggles[player.player_id] >= required_toggles:
+			completed += 1
+
+	progress_bar.value = total_progress / (players * required_toggles)
+
+	if completed >= players:
 		next_step()
+
 
 func update_dash_progress():
-	
-	var players_dashed = {}
+	var completed := 0
+	var total_progress := 0.0
 
 	for player in playermanager.players_container.get_children():
-		if Input.get_joy_axis(player.player_id,JOY_AXIS_TRIGGER_LEFT) >= 0.2:
-			players_dashed[player.player_id] = true
+		var trigger_pressed := Input.get_joy_axis(
+			player.player_id,
+			JOY_AXIS_TRIGGER_LEFT
+		) >= 0.2 
+		var not_deadzoned := Vector2(Input.get_joy_axis(player.player_id, JOY_AXIS_LEFT_X),
+		Input.get_joy_axis(player.player_id, JOY_AXIS_LEFT_Y)).length() >= 0.4
 
-	#progress.value = float(dashed_players.size()) / players * 100
+		if trigger_pressed and not_deadzoned:
+			dashed_players[player.player_id] = true
 
-	if players_dashed.size() >= players:
+		if dashed_players[player.player_id]:
+			total_progress += 1
+			completed += 1
+
+	progress_bar.value = total_progress / players
+
+	if completed >= players:
 		next_step()
 	
+func update_aim_progress(delta):
+	var aim_goal := 1.0
+
+	var completed := 0
+	var sum := 0.0
+
+	for player in playermanager.players_container.get_children():
+		var input := Vector2(
+			Input.get_joy_axis(player.player_id, JOY_AXIS_RIGHT_X),
+			Input.get_joy_axis(player.player_id, JOY_AXIS_RIGHT_Y)
+		)
+
+		if input.length() > 0.5:
+			aim_progress[player.player_id] += delta
+
+		var progress = min(aim_progress[player.player_id], aim_goal)
+		sum += progress
+
+		if aim_progress[player.player_id] >= aim_goal:
+			completed += 1
+
+	progress_bar.value = sum / (players * aim_goal)
+
+	if completed >= players:
+		next_step()
+	
+
 func update_bite_progress():
-	var players_dashed = {}
+	var completed := 0
+	var total_progress := 0.0
 
-	for player in playermanager.players_container.get_children():
-		if Input.get_joy_axis(player.player_id,JOY_AXIS_TRIGGER_RIGHT) >= 0.2:
-			players_dashed[player.player_id] = true
+	for player_id in bitten_players:
+		if bitten_players[player_id]:
+			total_progress += 1
+			completed += 1
 
-	#progress.value = float(dashed_players.size()) / players * 100
+	progress_bar.value = total_progress / players
 
-	if players_dashed.size() >= players:
+	if completed >= players:
 		next_step()
+
+func _on_bite_performed(player_id):
+	bitten_players[player_id] = true
 
 func setup_final_step():
 	decoy = decoy_scene.instantiate()
 
 	decoy.position = Vector2(400, 400)
 	decoy.required_players = players
-
+	decoy.finished.connect(finish)
+	
 	add_child(decoy)
 
-	decoy.finished.connect(finish)
+
+
+func update_final_progress():
+	if decoy == null:
+		return
+	
+	progress_bar.value = float(decoy.players_bitten.size()) / players
 
 func finish():
 	show_message("You did it!")
