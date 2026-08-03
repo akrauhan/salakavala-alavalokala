e6168f1e4f2634b46aa994f98d915a6a0cee1da8
akrauhan <a.rauhansalo@gmail.com>
2026-08-03 21:19:30 +0300

    added mainmenu amount controller handling, lost mouse handling xd

--- mainmenu.gd
+++ mainmenu.gd
@@ -1,45 +1,51 @@
 extends Control
 
-@onready var player_count := $VBoxContainer/HBoxContainer/SpinBox
-@onready var win_limit := $VBoxContainer/HBoxContainer2/SpinBox
+@onready var player_count := $VBoxContainer/PlayerCount
+@onready var win_limit := $VBoxContainer/WinLimit
 @onready var flapper := $Kala/FlapperAnimation
 @onready var flapper2 := $Kala2/FlapperAnimation
 @onready var start_button := $VBoxContainer/StartButton
-@onready var spin_box: SpinBox = $VBoxContainer/HBoxContainer/SpinBox
-@onready var spin_box2: SpinBox = $VBoxContainer/HBoxContainer2/SpinBox
+
+var active_spinbox: SpinBox = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	BackgroundMusic.play()
 	flapper.play()
 	flapper2.play()
 	start_button.grab_focus()
-	spin_box.value = min(Input.get_connected_joypads().size(), 2)
-	spin_box2.value = GameSettings.win_limit
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
+	if active_spinbox == null:
+		return
 
-	pass
+	if Input.is_action_just_pressed("ui_left"):
+		active_spinbox.value -= active_spinbox.step
+
+	elif Input.is_action_just_pressed("ui_right"):
+		active_spinbox.value += active_spinbox.step
+		
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		print("ui_accept")
 
 func _on_start_button_pressed() -> void:
-	GameSettings.player_count = int(player_count.value)
-	GameSettings.win_limit = int(win_limit.value)
+	GameSettings.player_count = player_count.value
+	GameSettings.win_limit = win_limit.value
 	get_tree().change_scene_to_file("res://gamemodes/versus.tscn") # Replace with function body.
 	queue_free()
 
 
 func _on_quitbutton_pressed() -> void:
 	get_tree().quit()
-	
 
 
-func _on_tutorial_button_pressed() -> void:
-	GameSettings.player_count = int(player_count.value)
-	get_tree().change_scene_to_file("res://gamemodes/tutorial.tscn")
-	pass # Replace with function body.
+
+func _on_player_count_spin_box_focus_entered() -> void:
+	active_spinbox = player_count
+
+func _on_player_count_spin_box_focus_exited() -> void:
+	if active_spinbox == player_count:
+		active_spinbox = null
