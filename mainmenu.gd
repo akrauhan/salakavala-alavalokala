9cc6b44ee804c2b42f65eff5b74a3f19eec6054f
akrauhan <a.rauhansalo@gmail.com>
2026-08-04 20:01:35 +0300

    main menu usable now with controllers, win limit still setting wrong amount of wins

--- mainmenu.gd
+++ mainmenu.gd
@@ -1,12 +1,15 @@
 extends Control
 
-@onready var player_count := $VBoxContainer/PlayerCount
-@onready var win_limit := $VBoxContainer/WinLimit
+@onready var player_count_button := $VBoxContainer/PlayerCount/PlayerCountButton
+@onready var win_limit_button := $VBoxContainer/WinLimit/WinLimitButton
 @onready var flapper := $Kala/FlapperAnimation
 @onready var flapper2 := $Kala2/FlapperAnimation
 @onready var start_button := $VBoxContainer/StartButton
 
 var active_spinbox: SpinBox = null
+
+var win_limit 
+var player_count
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,26 +17,21 @@ func _ready() -> void:
 	flapper.play()
 	flapper2.play()
 	start_button.grab_focus()
+	
+	player_count = GameSettings.player_count
+	win_limit = GameSettings.win_limit
 
 
 func _process(delta):
-	if active_spinbox == null:
-		return
-
-	if Input.is_action_just_pressed("ui_left"):
-		active_spinbox.value -= active_spinbox.step
-
-	elif Input.is_action_just_pressed("ui_right"):
-		active_spinbox.value += active_spinbox.step
-		
+	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		print("ui_accept")
 
 func _on_start_button_pressed() -> void:
-	GameSettings.player_count = player_count.value
-	GameSettings.win_limit = win_limit.value
+	GameSettings.player_count = player_count
+	GameSettings.win_limit = win_limit
 	get_tree().change_scene_to_file("res://gamemodes/versus.tscn") # Replace with function body.
 	queue_free()
 
@@ -41,11 +39,39 @@ func _on_start_button_pressed() -> void:
 func _on_quitbutton_pressed() -> void:
 	get_tree().quit()
 
+func change_player_count(amount):
+	player_count += amount
+	if player_count == 1:
+		player_count = 8
+	elif player_count == 9:
+		player_count = 2
+	player_count_button.text = str(player_count)
+	
+func change_win_limit_count(amount):
+	win_limit += amount
+	if win_limit == 0:
+		win_limit = 30
+	elif win_limit == 31:
+		win_limit = 1
+	win_limit_button.text = str(win_limit)
+	
 
 
-func _on_player_count_spin_box_focus_entered() -> void:
-	active_spinbox = player_count
+func _on_player_count_button_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_right"):
+		change_player_count(1)
+		accept_event()
+	if event.is_action_pressed("ui_left"):
+		change_player_count(-1)
+		accept_event()
 
-func _on_player_count_spin_box_focus_exited() -> void:
-	if active_spinbox == player_count:
-		active_spinbox = null
+
+
+func _on_win_limit_button_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_right"):
+		change_win_limit_count(1)
+		accept_event()
+	if event.is_action_pressed("ui_left"):
+		change_win_limit_count(-1)
+		accept_event()
+	
